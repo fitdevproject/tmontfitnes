a360d56457bec1db71c73a1b58aeda9aaad6a635
fitdevproject <fitdevproject@gmail.com>
2022-01-03 23:27:33 -0500

    Updated macro calculator

--- pages/FreeResources/index.tsx
+++ pages/FreeResources/index.tsx
@@ -9,7 +9,7 @@ const FreeResources: NextPage = () => {
       <div className=" my-16 text-center">
         <Link href="https://tmontfitness.gumroad.com/l/UOSFf?_ga=2.212175188.1582768222.1639430290-841510740.1630159065&_gl=1*1eu84co*_ga*ODQxNTEwNzQwLjE2MzAxNTkwNjU.*_ga_6LJN6D94N6*MTYzOTQzMDI4OS40LjEuMTYzOTQzMDgxNy4w">
           <a
-            className="mx-auto block my-4 rounded bg-gradient-to-r from-green-500 to-green-700 max-w-xs md:max-w-xl py-4 px-4 text-sm md:text-lg hover:bg-gradient-to-r hover:from-green-600 hover:to-green-900 hover:text-black text-neutral-700"
+            className="mx-auto block my-4 rounded bg-gradient-to-r from-green-500 to-green-700 max-w-xs md:max-w-xl py-4 px-4 md:text-lg hover:bg-gradient-to-r hover:from-green-600 hover:to-green-900 hover:text-black text-neutral-700"
             target="_blank"
           >
             Free Full Body Workout Blueprint
--- pages/MacroCalculator/index.jsx
+++ pages/MacroCalculator/index.jsx
@@ -93,104 +93,189 @@ const MacroCalculator = () => {
       </div>
     );
   };
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  useEffect(() => {}, [dailyCalories, dailyProtein]);
   return (
     <>
       <h1 className="block mx-auto my-4 text-center text-neutral-50 text-3xl">
         Macro Calculator
       </h1>
-      <Formik
-        initialValues={{
-          gender: "",
-          goal: "",
-          activityLevel: "",
-          currentWeight: "",
-          goalWeight: "",
-        }}
-        validationSchema={Yup.object({
-          gender: Yup.string().required("Gender is Required"),
-          goal: Yup.string().required("Goal is Required"),
-          activityLevel: Yup.string().required("Activity Level is Required"),
-          currentWeight: Yup.number()
-            .typeError("Enter a valid number")
-            .required("Current Weight is Required"),
-          goalWeight: Yup.number()
-            .typeError("Enter a valid number")
-            .required("Goal Weight is Required"),
-        })}
-        onSubmit={(values, { resetForm }) => {
-          alert(JSON.stringify(values, null, 2));
-          calculateCalories(
-            values.goal,
-            values.goalWeight,
-            values.currentWeight,
-            values.activityLevel
-          );
-          calculateProteinGoal(values.gender, values.goalWeight);
-          alert(
-            "You're calorie goal is:" +
-              dailyCalories +
-              " and your daily protein goal is: " +
-              dailyProtein
-          );
-          resetForm();
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="flex flex-col items-center">
-            <MySelect label="Gender" name="gender">
-              <option value="">Select a gender</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </MySelect>
-            <MySelect label="Goal" name="goal">
-              <option value="">Select a goal</option>
-              <option value="Lose Fat">
-                Lost Fat - Lose 1-2 pounds per week
-              </option>
-              <option value="Build Muscle">
-                Build Muscle - Gain 1-2 pounds per week
-              </option>
-              <option value="Maintain">
-                Maintain - Maintain my current weight
-              </option>
-            </MySelect>
-            <MySelect label="Activity Level" name="activityLevel">
-              <option value="">Select an activity level</option>
-              <option value="Sedentary">
-                Sedentary - Little or no exercise
-              </option>
-              <option value="Lightly Active">
-                Lightly Active - Exercise 1-2 days per week
-              </option>
-              <option value="Moderately Active">
-                Moderately Active - Exercise 3-5 days per week
-              </option>
-              <option value="Extremely Active">
-                Extremely Active - Exercise 6-7 days per week
-              </option>
-            </MySelect>
-            <MyTextInput
-              label="Current Weight"
-              name="currentWeight"
-              type="text"
-              placeholder="Enter current weight"
-            />
-            <MyTextInput
-              label="Goal Weight"
-              name="goalWeight"
-              type="text"
-              placeholder="Enter goal weight"
-            />
-            <button
-              disabled={isSubmitting}
-              type="submit"
-              className="w-60 my-6 py-2 px-4 rounded bg-gradient-to-r from-green-500 to-green-700 text-neutral-700 hover:bg-gradient-to-r hover:from-green-600 hover:to-green-900 hover:text-black"
-            >
-              Calculate
-            </button>
-          </Form>
-        )}
-      </Formik>
+      {dailyCalories === 0 && dailyProtein === 0 ? (
+        <Formik
+          initialValues={{
+            gender: "",
+            goal: "",
+            activityLevel: "",
+            currentWeight: "",
+            goalWeight: "",
+          }}
+          validationSchema={Yup.object({
+            gender: Yup.string().required("Gender is Required"),
+            goal: Yup.string().required("Goal is Required"),
+            activityLevel: Yup.string().required("Activity Level is Required"),
+            currentWeight: Yup.number()
+              .typeError("Enter a valid number")
+              .required("Current Weight is Required"),
+            goalWeight: Yup.number()
+              .typeError("Enter a valid number")
+              .required("Goal Weight is Required"),
+          })}
+          onSubmit={(values, { resetForm }) => {
+            calculateCalories(
+              values.goal,
+              values.goalWeight,
+              values.currentWeight,
+              values.activityLevel
+            );
+            calculateProteinGoal(values.gender, values.goalWeight);
+
+            resetForm();
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="flex flex-col items-center">
+              <MySelect label="Gender" name="gender">
+                <option value="">Select a gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </MySelect>
+              <MySelect label="Goal" name="goal">
+                <option value="">Select a goal</option>
+                <option value="Lose Fat">
+                  Lost Fat - Lose 1-2 pounds per week
+                </option>
+                <option value="Build Muscle">
+                  Build Muscle - Gain 1-2 pounds per week
+                </option>
+                <option value="Maintain">
+                  Maintain - Maintain my current weight
+                </option>
+              </MySelect>
+              <MySelect label="Activity Level" name="activityLevel">
+                <option value="">Select an activity level</option>
+                <option value="Sedentary">
+                  Sedentary - Little or no exercise
+                </option>
+                <option value="Lightly Active">
+                  Lightly Active - Exercise 1-2 days per week
+                </option>
+                <option value="Moderately Active">
+                  Moderately Active - Exercise 3-5 days per week
+                </option>
+                <option value="Extremely Active">
+                  Extremely Active - Exercise 6-7 days per week
+                </option>
+              </MySelect>
+              <MyTextInput
+                label="Current Weight"
+                name="currentWeight"
+                type="text"
+                placeholder="Enter current weight"
+              />
+              <MyTextInput
+                label="Goal Weight"
+                name="goalWeight"
+                type="text"
+                placeholder="Enter goal weight"
+              />
+              <button
+                disabled={isSubmitting}
+                type="submit"
+                className="w-60 my-6 py-2 px-4 rounded bg-gradient-to-r from-green-500 to-green-700 text-neutral-700 hover:bg-gradient-to-r hover:from-green-600 hover:to-green-900 hover:text-black"
+              >
+                Calculate
+              </button>
+            </Form>
+          )}
+        </Formik>
+      ) : (
+        <div className="flex flex-col items-center">
+          <div className="bg-neutral-400 px-4 py-4 w-80 md:w-2/3 md:h-60 md:flex md:flex-col md:justify-evenly rounded mb-6">
+            <div className="mb-2">
+              <h1 className="text-neutral-800 text-2xl text-center">Results</h1>
+              <hr className="mt-4" />
+            </div>
+            <div className="md:flex md:items-center justify-evenly">
+              <div className="px-2 py-2 flex md:flex-col">
+                <h1 className="text-neutral-800 mr-2 md:m-0">
+                  Daily Calorie Target:
+                </h1>
+                <p className="text-neutral-800 text-center font-bold">
+                  {dailyCalories}
+                </p>
+              </div>
+              <div className="px-2 py-2 flex md:flex-col">
+                <h1 className="text-neutral-800 mr-2 md:m-0">
+                  Daily Protein Target:
+                </h1>
+                <p className="text-neutral-800 text-center font-bold">
+                  {dailyProtein}
+                </p>
+              </div>
+              <div className="px-2 py-2 flex md:flex-col">
+                <h1 className="text-neutral-800 mr-2 md:m-0">
+                  Weekly Calorie Target:
+                </h1>
+                <p className="text-neutral-800 text-center font-bold">
+                  {numberWithCommas(dailyCalories * 7)}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="px-4 py-4 w-96 md:w-4/5 mb-8">
+            <p className=" text-xl font-bold text-green-500">Calorie Target:</p>
+            <p className="mt-2 text-neutral-50 mb-4">
+              Your daily calorie goal is the amount of calories you should aim
+              to eat each day. Your total calories for the week has also been
+              calculated and shown above. Knowing your weekly caloric intake is
+              a very powerful tool, because it allows you to have days where you
+              either consume less than or more than your calorie goal for the
+              day and still be compliant to your overall calorie goal. This is
+              very useful in scenarios where life happens, and you simply just
+              want to enjoy yourself. No harm no foul, since you know your total
+              calorie goal for the week.
+            </p>
+            <p className=" text-xl font-bold text-green-500">Protein Target:</p>
+            <p className="mt-2 text-neutral-50 mb-4">
+              Your daily protein target is the most important number to hit.
+              Calories from protein should be prioritized, because they will
+              help you feel more full throughout each day. Protein is also the
+              building block for your muscles. It is going to be important to
+              hit your protein goal each day to either hold onto or gain muscle
+              depending on the goal you have chosen.
+            </p>
+            <p className="text-green-500 text-xl font-bold">
+              Fat & Carb Target:
+            </p>
+            <p className="mt-2 text-neutral-50 mb-4">
+              Your daily fat & carb target is up to you. As long as you are
+              hitting your protein and calorie goal, the amount of carb vs fat
+              doesn't matter. Some do better with higher carb, and some do
+              better with lower carb. We are following more of a flexible
+              approach to dieting, because I don't believe in forcing someone
+              into a super restrictive lifestyle by following a diet where a
+              macro nutrient is entirely cut out for the sake of losing weight.
+            </p>
+            <p className="text-sm text-neutral-50 mb-4">
+              * This calculation is an estimation based on the information you
+              have provided. If your goal is to lose fat or build muscle and you
+              are gaining or losing too quickly, add or subtract 100 calories
+              per week until you are losing or gaining 1-2lbs per week. If your
+              goal is to maintain and you are gaining or losing vice
+              maintaining, add or subtract 100 calories per week until you are
+              maintaining.
+            </p>
+            <p className="text-sm text-neutral-50">
+              * You should always consult with a health professional before
+              following any diet.
+            </p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
